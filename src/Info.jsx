8322560a6458dd7c47af46b1b5927e85391c1fcb
Fran Paez <99830348+gryllifish@users.noreply.github.com>
2023-03-29 18:06:58 -0300

    prueba

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { FaEnvelopeSquare } from 'react-icons/fa';
 import { FaLinkedin} from 'react-icons/fa';
-import { imagen } from "./images/fran.jpg";
+import { imagen } from "./src/assets/fran.jpg";
 
 
 export default function Info(){
